d3155c8184bc86d1ad5680fa16b81f2c86fea18d
eduar <eduardo17silva05@gmail.com>
2025-03-21 09:15:41 +0000

    v2

--- src/main/kotlin/LCD.kt
+++ src/main/kotlin/LCD.kt
@@ -9,19 +9,60 @@ object LCD {
     private const val SERIAL_INTERFACE = false
 
     // Escreve um byte de comando/dados no LCD em paralelo
-    private fun writeNibbleParallel(rs: Boolean, data: Int) {}
+    private fun writeNibbleParallel(rs: Boolean, data: Int){
+
+    }
 
     // Escreve um byte de comando/dados no LCD em serie
-    private fun writeNibbleSerial(rs: Boolean, data: Int) {}
+    private fun writeNibbleSerial(rs: Boolean, data: Int){
+
+    }
 
     // Escreve um nibble de comando/dados no LCD
-    private fun writeNibble(rs: Boolean, data: Int) {}
+    private fun writeNibble(rs: Boolean, data: Int){
+    if(SERIAL_INTERFACE) writeNibbleSerial(rs, data)
+    else writeNibbleParallel(rs, data)
+    }
 
     // Escreve um byte de comando/dados no LCD
-    private fun writeByte(rs: Boolean, data: Int) {}
+    private fun writeByte(rs: Boolean, data: Int){
+
+    }
 
     // Escreve um comando no LCD
-    private fun writeCMD(data: Int) {}
+    private fun writeCMD(data: Int){
+
+    }
+
+    // Escreve um dado no LCD
+    private fun writeDATA(data: Int){
+
+    }
+
+    // Envia a sequencia de iniciacao para comunicacao a 4 bits.
+    fun init (){
+
+    }
+
+    // Escreve um carater na posicao corrente.
+    fun write (c: Char){
+
+    }
+
+    // Escreve uma string na posicao corrente.
+    fun write(text: String){
+
+    }
+
+    // Envia comando para posicionar cursor ('line ' : 0. . LINES -1 , 'column ' : 0. . COLS -1)
+    fun cursor (line: Int, column: Int){
+
+    }
+
+    // Envia comando para limpar o ecra e posicionar o cursor em (0,0)
+    fun clear (){
+
+    }
 }
 
 
